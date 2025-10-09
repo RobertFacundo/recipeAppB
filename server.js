7bdf01217cb08e1125113e3f92605874c7c4bb97
Facundo Robert <robert.facundo@hotmail.com>
2025-10-08 23:34:33 -0300

    !

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-// server.js (Versión Modular)
-
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'; 
-import connectDB from './db.js'; // <-- Importamos la función desde el nuevo archivo
+import connectDB from './db.js'; 
 
 const app = express();
-// MONGODB_URI ya no se necesita aquí.
 const PORT = process.env.PORT || 3000;
 
-// ====================================
-// MIDDLEWARE Y RUTAS
-// ====================================
-
-app.use(express.json()); // Permite a Express leer el cuerpo de las peticiones JSON
+app.use(express.json()); 
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -22,25 +15,19 @@ app.get('/', (req, res) => {
     });
 });
 
-// ====================================
-// INICIAR CONEXIÓN Y SERVIDOR
-// ====================================
 
 const startServer = async () => {
     try {
-        // 1. Intentar conectar a la DB
         await connectDB(); 
 
-        // 2. Si la DB conecta, iniciamos el servidor Express
         app.listen(PORT, () => {
             console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
         });
 
     } catch (error) {
-        // Si connectDB lanza un error, cerramos el proceso.
         console.error("No se pudo iniciar el servidor debido a un error en la base de datos.");
         process.exit(1); 
     }
 }
 
-startServer(); // Llamar a la función principal
+startServer(); 
